c3fbeea5cb10ba71ee50e9c75177a42b3470430e
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-09-01 20:20:19 -0300

    chore: removed fresh default assets

--- routes/index.tsx
+++ routes/index.tsx
@@ -8,15 +8,8 @@ export default function Home() {
       <Head>
         <title>conhecaoespiritosanto</title>
       </Head>
-      <div class="px-4 py-8 mx-auto bg-[#86efac]">
+      <div class="px-4 py-8 mx-auto bg-azul">
         <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-          <img
-            class="my-6"
-            src="/logo.svg"
-            width="128"
-            height="128"
-            alt="the fresh logo: a sliced lemon dripping with juice"
-          />
           <h1 class="text-4xl font-bold">Welcome to fresh</h1>
           <p class="my-4">
             Try updating this message in the

a4afbec274378afad9f1d3695fb9c34dbd7d94cf
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-08-31 22:39:32 -0300

    chore: add color pallette to twind.config.ts

--- twind.config.ts
+++ twind.config.ts
@@ -5,6 +5,16 @@ import presetAutoprefix from "https://esm.sh/@twind/preset-autoprefix@1.0.7";
 export default {
   ...defineConfig({
     presets: [presetTailwind() as Preset, presetAutoprefix()],
+    theme: {
+      colors: {
+        branco: "rgb(255, 255, 255)",
+        preto: "rgb(0, 0, 0)",
+        azul: "rgb(100, 180, 240)",
+        "azul-claro": "rgb(135, 206, 235)",
+        rosa: "rgb(255, 174, 200)",
+        "rosa-claro": "rgb(255, 192, 203)",
+      },
+    },
   }),
   selfURL: import.meta.url,
 };

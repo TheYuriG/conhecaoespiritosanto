9ee769c28d9bbc4f2af1be4854063b263f2ca5f1
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-09-10 17:02:09 -0300

    refactor: externally define color pallete for reuse

--- twind.config.ts
+++ twind.config.ts
@@ -2,6 +2,16 @@ import { defineConfig, Preset } from "https://esm.sh/@twind/core@1.1.3";
 import presetTailwind from "https://esm.sh/@twind/preset-tailwind@1.1.4";
 import presetAutoprefix from "https://esm.sh/@twind/preset-autoprefix@1.0.7";
 
+// Define paleta de cores do site
+const colors = {
+  branco: "rgb(255, 255, 255)",
+  preto: "rgb(0, 0, 0)",
+  azul: "rgb(100, 180, 240)",
+  "azul-claro": "rgb(135, 206, 235)",
+  rosa: "rgb(255, 174, 200)",
+  "rosa-claro": "rgb(255, 192, 203)",
+};
+
 export default {
   ...defineConfig({
     presets: [presetTailwind() as Preset, presetAutoprefix()],
@@ -10,14 +20,7 @@ export default {
         "url('https://fonts.googleapis.com/css2?family=Agbalumo&family=Rubik&display=swap');",
     },
     theme: {
-      colors: {
-        branco: "rgb(255, 255, 255)",
-        preto: "rgb(0, 0, 0)",
-        azul: "rgb(100, 180, 240)",
-        "azul-claro": "rgb(135, 206, 235)",
-        rosa: "rgb(255, 174, 200)",
-        "rosa-claro": "rgb(255, 192, 203)",
-      },
+      colors,
     },
     rules: [
       ["font-agba", {

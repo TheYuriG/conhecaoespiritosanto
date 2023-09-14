261abaff704ec7db4f5265d22404ebe8a1e7c055
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-09-14 12:22:03 -0300

    refactor: import color theme from file

--- twind.config.ts
+++ twind.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig, Preset } from "https://esm.sh/@twind/core@1.1.3";
 import presetTailwind from "https://esm.sh/@twind/preset-tailwind@1.1.4";
 import presetAutoprefix from "https://esm.sh/@twind/preset-autoprefix@1.0.7";
+import { colors } from "./data/colors.ts";
 
 // Define paleta de cores do site
-const colors = {
-  branco: "rgb(255, 255, 255)",
-  preto: "rgb(0, 0, 0)",
-  azul: "rgb(100, 180, 240)",
-  "azul-claro": "rgb(135, 206, 235)",
-  rosa: "rgb(255, 174, 200)",
-  "rosa-claro": "rgb(255, 192, 203)",
-};
 
 export default {
   ...defineConfig({

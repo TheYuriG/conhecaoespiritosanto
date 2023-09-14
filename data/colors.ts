539f6aa585f524eb232faab3bc987b2d4af6a330
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-09-14 12:14:21 -0300

    feat: added near-white shades

--- data/colors.ts
+++ data/colors.ts
@@ -7,8 +7,10 @@ export const colors = {
   // Azuis
   azul: "rgb(100, 180, 240)", // Azul oficial da bandeira do Espírito Santo
   "azul-claro": "rgb(135, 206, 235)", // Azul oficial alguns tons mais claro
+  "azul-clarissimo": "rgb(240, 240, 250)", // Branco azulado
 
   // Rosas
   rosa: "rgb(255, 174, 200)", // Rosa oficial da bandeira do Espírito Santo
   "rosa-claro": "rgb(255, 192, 203)", // Rosa oficial alguns tons mais claro
+  "rosa-clarissimo": "rgb(255, 235, 240)", // Branco rosado
 };

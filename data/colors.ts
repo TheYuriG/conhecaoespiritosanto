23cc819def08b40c7605eaa65bb42202ec716629
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-09-14 12:29:28 -0300

    feat: added darker color shades

--- data/colors.ts
+++ data/colors.ts
@@ -8,9 +8,11 @@ export const colors = {
   azul: "rgb(100, 180, 240)", // Azul oficial da bandeira do Espírito Santo
   "azul-claro": "rgb(135, 206, 235)", // Azul oficial alguns tons mais claro
   "azul-clarissimo": "rgb(240, 240, 250)", // Branco azulado
+  "azul-escuro": "rgb(50, 50, 240)", // Azul oficial alguns tons mais escuro
 
   // Rosas
   rosa: "rgb(255, 174, 200)", // Rosa oficial da bandeira do Espírito Santo
   "rosa-claro": "rgb(255, 192, 203)", // Rosa oficial alguns tons mais claro
   "rosa-clarissimo": "rgb(255, 235, 240)", // Branco rosado
+  "rosa-escuro": "rgb(255, 134, 160)", // Rosa oficial alguns tons mais escuro
 };

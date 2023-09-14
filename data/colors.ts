41b944bb3eaf3d20520c455d7372e9cf0281b9d3
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-09-14 12:09:21 -0300

    refactor: moved theme colors to separate file

---
+++ data/colors.ts
@@ -0,0 +1,14 @@
+// Define cores a serem usadas num arquivo separado para ser importado em outros arquivos
+export const colors = {
+  // Extremos
+  branco: "rgb(255, 255, 255)",
+  preto: "rgb(0, 0, 0)",
+
+  // Azuis
+  azul: "rgb(100, 180, 240)", // Azul oficial da bandeira do Espírito Santo
+  "azul-claro": "rgb(135, 206, 235)", // Azul oficial alguns tons mais claro
+
+  // Rosas
+  rosa: "rgb(255, 174, 200)", // Rosa oficial da bandeira do Espírito Santo
+  "rosa-claro": "rgb(255, 192, 203)", // Rosa oficial alguns tons mais claro
+};

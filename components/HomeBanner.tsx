7facd74088e22a314e75060ecd7d6d5b146f58cd
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-09-14 11:16:21 -0300

    refactor: apply Card to HomeBanner

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -1,5 +1,7 @@
 // SVG do contorno do estado do Espírito Santo
 import { EsSvg } from "../assets/EsSvg.tsx";
+// Abstração de um Card
+import { Card } from "./UI/card.tsx";
 
 // Exporta o componente do banner da home page
 export function HomeBanner() {
@@ -8,7 +10,7 @@ export function HomeBanner() {
       {/* Linha com texto e icone do estado */}
       <div class="mx-2 md:mx-4 h-full flex items-center justify-center">
         {/* Coluna com Título e Texto */}
-        <div class="px-4 py-2 lg:ml-16 flex flex-col items-center justify-center rounded-lg bg-branco">
+        <Card backgroundColor="azul-clarissimo" shadowColor="rosa-claro">
           {/* Título */}
           <h1 class="text-4xl font-bold text-rosa font-agba text-shadow-azul-claro">
             Vamos conhecer o Espírito Santo?
@@ -19,7 +21,7 @@ export function HomeBanner() {
             surpreender! Preparado para descobrir o quanto você conhece deste
             pedacinho do Sudeste?
           </p>
-        </div>
+        </Card>
         {/* Ícone do estado */}
         <EsSvg
           stroke="blue"

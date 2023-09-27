626311710fc8454a7a8e43f5efc33bb4ecb96344
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-09-27 20:34:36 -0300

    feat: add home features to index

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,16 +2,22 @@
 import { CustomHead } from "../components/CustomHead.tsx";
 // Banner da página inicial com CTA
 import { HomeBanner } from "../components/HomeBanner.tsx";
+// Estatísticas do site
+import { HomeFeatures } from "../components/HomeFeatures.tsx";
 
 export default function Home() {
   return (
     <>
+      {/* Head para SEO */}
       <CustomHead
         description="Bem vindo ao melhor site de turismo capixaba. Conheça os pontos turísticos mais importantes do estado e os diversos pontos de apoio que os rodeiam."
         title="Bem Vindo!"
         children={[]}
       />
+      {/* Banner de boas-vindas */}
       <HomeBanner />
+      {/* Informaçoes de rotas listadas */}
+      <HomeFeatures />
     </>
   );
 }

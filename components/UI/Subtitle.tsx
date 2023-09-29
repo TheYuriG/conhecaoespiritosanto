8fa778765d491ce806c8e91ac2709763c353bfd2
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-09-29 07:29:33 -0300

    fix: centered title and subtitle

--- components/UI/Subtitle.tsx
+++ components/UI/Subtitle.tsx
@@ -1,7 +1,7 @@
 // Cria um subtítulo com fonte maior
 export function Subtitle({ content }: { content: string }) {
   return (
-    <h2 class="text-2xl font-bold text-rosa font-agba text-shadow-azul-escuro">
+    <h2 class="text-2xl text-center font-bold text-rosa font-agba text-shadow-azul-escuro">
       {content}
     </h2>
   );
--- components/UI/Title.tsx
+++ components/UI/Title.tsx
@@ -1,7 +1,7 @@
 // Cria um título com fonte maior
 export function Title({ content }: { content: string }) {
   return (
-    <h1 class="text-4xl font-bold text-rosa font-agba text-shadow-azul-escuro">
+    <h1 class="text-4xl text-center font-bold text-rosa font-agba text-shadow-azul-escuro">
       {content}
     </h1>
   );

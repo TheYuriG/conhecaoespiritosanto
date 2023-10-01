b810bdc0735894345ade0443ae810542f56377a2
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-10-01 11:01:29 -0300

    feat: added EmailSvg

---
+++ assets/EmailSvg.tsx
@@ -0,0 +1,20 @@
+// Exporta um componente de um SVG de email. Permite customização de classes e a cor do contorno. Extraído de TablerIcons.
+export function EmailSvg(
+  { stroke, classes }: { stroke: string; classes?: string },
+) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      stroke-width="2"
+      class={classes}
+      fill="none"
+      stroke={stroke}
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" />
+      <path d="M3 7l9 6l9 -6" />
+    </svg>
+  );
+}

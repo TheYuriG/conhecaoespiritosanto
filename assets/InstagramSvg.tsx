21c7f00f1a7dae87498c1b0c07bf6da4ffe4f04a
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-10-02 14:24:18 -0300

    feat: added InstagramSvg

---
+++ assets/InstagramSvg.tsx
@@ -0,0 +1,21 @@
+// Exporta um componente de um SVG de um ícone do Instagram. Permite customização de classes e a cor do contorno. Extraído de TablerIcons.
+export function InstagramSvg(
+  { stroke, classes }: { stroke: string; classes?: string },
+) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      stroke-width="2"
+      class={classes}
+      fill="none"
+      stroke={stroke}
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
+      <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+      <path d="M16.5 7.5l0 .01" />
+    </svg>
+  );
+}

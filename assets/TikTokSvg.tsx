4962c6e96f01f628329bab4fe971f3d48f443980
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-10-02 14:26:50 -0300

    feat: added TikTokSvg

---
+++ assets/TikTokSvg.tsx
@@ -0,0 +1,19 @@
+// Exporta um componente de um SVG de um ícone do TikTok. Permite customização de classes e a cor do contorno. Extraído de TablerIcons.
+export function TikTokSvg(
+  { stroke, classes }: { stroke: string; classes?: string },
+) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      stroke-width="2"
+      class={classes}
+      fill="none"
+      stroke={stroke}
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path d="M21 7.917v4.034a9.948 9.948 0 0 1 -5 -1.951v4.5a6.5 6.5 0 1 1 -8 -6.326v4.326a2.5 2.5 0 1 0 4 2v-11.5h4.083a6.005 6.005 0 0 0 4.917 4.917z" />
+    </svg>
+  );
+}

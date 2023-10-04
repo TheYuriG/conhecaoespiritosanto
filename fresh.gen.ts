82e2ede70c387e0face4bb2455c1c20eb70a9b07
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-10-04 11:29:43 -0300

    feat: added footer to all pages layout

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,12 +3,14 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $0 from "./routes/_404.tsx";
-import * as $1 from "./routes/index.tsx";
+import * as $1 from "./routes/_layout.tsx";
+import * as $2 from "./routes/index.tsx";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
-    "./routes/index.tsx": $1,
+    "./routes/_layout.tsx": $1,
+    "./routes/index.tsx": $2,
   },
   islands: {},
   baseUrl: import.meta.url,
---
+++ routes/_layout.tsx
@@ -0,0 +1,13 @@
+import { LayoutProps } from "$fresh/server.ts";
+// Rodapé a ser adicionado a todas as páginas
+import Footer from "../components/UI/Footer.tsx";
+
+// Cria um layout comum que injeta conteúdo de páginas acessadas
+export default function Layout({ Component }: LayoutProps) {
+  return (
+    <div class="layout">
+      <Component />
+      <Footer />
+    </div>
+  );
+}

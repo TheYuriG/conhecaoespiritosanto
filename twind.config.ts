939eaa459885586ed10a18ba2ce62f49ff5860aa
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-10-07 07:12:51 -0300

    feat: added custom scrollbar to twind config

--- twind.config.ts
+++ twind.config.ts
@@ -1,9 +1,9 @@
+// Configurações básicas Twind V1
 import { defineConfig, Preset } from "https://esm.sh/@twind/core@1.1.3";
 import presetTailwind from "https://esm.sh/@twind/preset-tailwind@1.1.4";
 import presetAutoprefix from "https://esm.sh/@twind/preset-autoprefix@1.0.7";
+// Importa paleta de cores definida do site
 import { colors } from "./data/colors.ts";
-
-// Define paleta de cores do site
 
 export default {
   ...defineConfig({
@@ -16,6 +16,20 @@ export default {
       colors,
     },
     rules: [
+      ["custom-scrollbar", {
+        "&::-webkit-scrollbar-thumb": {
+          "background-color": colors["azul-claro"],
+          outline: "0.15rem solid " + colors["azul-escuro"],
+          "outline-offset": "-0.15rem",
+          "border-radius": "0.4rem",
+        },
+        "&::-webkit-scrollbar": {
+          width: "1em",
+        },
+        "&::-webkit-scrollbar-track": {
+          "background-color": colors["rosa-claro"],
+        },
+      }],
       ["font-agba", {
         "font-family": "Agbalumo",
       }],

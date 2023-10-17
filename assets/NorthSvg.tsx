f0f290f10b0874abc9523e675acc9226414065d6
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-10-17 18:16:22 -0300

    feat: added north SVG

---
+++ assets/NorthSvg.tsx
@@ -0,0 +1,20 @@
+// Exporta um componente de um SVG do símbolo de Norte. Permite customização de classes e a cor do contorno. Extraído de TablerIcons.
+export function NorthSvg(
+  { stroke, classes }: { stroke: string; classes?: string },
+) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      stroke-width="2"
+      class={classes}
+      fill="none"
+      stroke={stroke}
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+      <path d="M10 15v-6l4 6v-6" />
+    </svg>
+  );
+}

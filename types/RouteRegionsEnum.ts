e4fbb6aa136d03cb4a847b009a36f55b35c3e478
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-10-18 06:16:44 -0300

    chore: added route region enum

---
+++ types/RouteRegionsEnum.ts
@@ -0,0 +1,7 @@
+// Exporta ENUM com as quatro maiores regiões do Estado do Espírito Santo
+export enum RouteRegionsEnum {
+  NORTE = "Região Norte",
+  SUL = "Região Sul",
+  SERRANA = "Região Serrana",
+  GRANDE_VITORIA = "Grande Vitória",
+}

a2b620911a3f196a9367db5fa3dc4357d541d734
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-10-18 06:20:17 -0300

    chore: added route type enum

---
+++ types/RouteEntertainmentEnum.ts
@@ -0,0 +1,7 @@
+// Exporta ENUM com tipos de entretenimento possíveis para rotas marcadas no site
+export enum RouteEntertainmentEnum {
+  PONTO_TURISTICO = "Ponto Turístico",
+  PRAIA = "Praia",
+  RESTAURANTE = "Restaurante",
+  VIDA_NOTURNA = "Vida Noturna",
+}
